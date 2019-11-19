0f176897b2061794e235bb68428fb9ebb032f653
Iara <iarurodriguez981@gmail.com>
2019-11-19 20:55:03 -0300

    cambios de colores PIOLAS

--- src/ColoresMariano.java
+++ src/ColoresMariano.java
@@ -1,27 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author b4
- */
 public class ColoresMariano {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1 = "AZUL";
+        color1 = "FUXIA";
         color2  = "ROJO";
-        color3  = "VERDE";
+        color3  = "AMARILLO";
         System.out.printf("% es mi primer color favorito\n", color1);
         System.out.printf("%es mi segundo color favorito\n", color2);
         System.out.printf("%es mi tercer color favorito\n", color3);
-        // TODO code application logic here
+        
     }
     
 }
