0b30157c471290e19baca63120bf6822831520e4
b4 <b4@Ter74-W10.cen.local>
2019-11-19 21:44:15 -0300

    se amplia la lista a 4 colores!

--- src/ColoresMariano.java
+++ src/ColoresMariano.java
@@ -14,13 +14,16 @@ public class ColoresMariano {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String color1, color2, color3;
+        String color1, color2, color3, color4;
         color1 = "AZUL";
         color2  = "ROJO";
-        color3  = "VERDE";
+        color3  = "PURPURA";
+        color4  = "MAGENTA";
         System.out.printf("% es mi primer color favorito\n", color1);
+        System.out.printf("% no es mi color principal pero tambien me gusta\n", color2);
         System.out.printf("%es mi segundo color favorito\n", color2);
         System.out.printf("%es mi tercer color favorito\n", color3);
+        System.out.printf("%es mi ultimo color de la lista\n", color4);
         // TODO code application logic here
     }
     
